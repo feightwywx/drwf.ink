af9239a3c42425a338b2d11cc30ce4f71f62bfdf
feightwywx <kururinmiracle@outlook.com>
2022-12-01 00:49:21 +0800

    add(blog): code block

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,4 +1,4 @@
-import { Container, Divider, Link, Typography, useTheme } from "@mui/material";
+import { Container, Divider, Link, Typography, useMediaQuery, useTheme } from "@mui/material";
 import type { NextPage } from "next";
 import ReactMarkdown from "react-markdown";
 import { HeadLine } from "../../components/headline";
@@ -6,27 +6,33 @@ import { getAllPostIds, getPostData } from "../../utils/blog";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
+import TagIcon from '@mui/icons-material/Tag';
 
 import React from "react";
-import type { Typography as TypographyType, Variant } from "@mui/material/styles/createTypography";
+import type {
+  Typography as TypographyType,
+  Variant,
+} from "@mui/material/styles/createTypography";
+
+import { CopyBlock, atomOneDark, atomOneLight } from "react-code-blocks";
 
 const typographyStylesMapping = {
-  h1: 'headlineLarge',
-  h2: 'headlineMedium',
-  h3: 'headlineSmall',
-  h4: 'titleLarge',
-  h5: 'titleMedium',
-  h6: 'titleSmall',
-  subtitle1: 'subtitle1',
-  subtitle2: 'subtitle2',
-  body1: 'bodyLarge',
-  body2: 'bodyMedium',
-  button: 'labelLarge',
-  caption: 'caption',
-  overline: 'overline',
+  h1: "headlineLarge",
+  h2: "headlineMedium",
+  h3: "headlineSmall",
+  h4: "titleLarge",
+  h5: "titleMedium",
+  h6: "titleSmall",
+  subtitle1: "subtitle1",
+  subtitle2: "subtitle2",
+  body1: "bodyLarge",
+  body2: "bodyMedium",
+  button: "labelLarge",
+  caption: "caption",
+  overline: "overline",
 } as { [key in Variant]: keyof TypographyType };
 
-const Heading: React.FC = ({ children, id, level } : { id?: string }) => {
+const Heading: React.FC = ({ children, id, level }: { id?: string }) => {
   const variant = `h${Math.min(level, 6)}` as Variant;
   const theme = useTheme();
 
@@ -38,10 +44,33 @@ const Heading: React.FC = ({ children, id, level } : { id?: string }) => {
         ...(theme.typography[typographyStylesMapping[variant]] as Object),
         marginTop: "0.5em",
         marginBottom: "0.5em",
+        fontWeight: 600,
       }}
     >
-      <Link underline="hover" href={`#${id}`}>{children}</Link>
+      <TagIcon color="secondary" fontSize="small" />
+      <Link underline="hover" href={`#${id}`}>
+        {children}
+      </Link>
     </Typography>
+  );
+};
+
+const Code: React.FC = ({ node, inline, className, children, ...props }) => {
+  const match = /language-(\w+)/.exec(className || "");
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  return !inline ? (
+    <CopyBlock
+      text={children}
+      language={match ? match[1] : undefined}
+      theme={prefersDarkMode ? atomOneDark : atomOneLight}
+      showLineNumbers
+      codeBlock
+    />
+  ) : (
+    <code className={className} {...props}>
+      {children}
+    </code>
   );
 };
 
@@ -66,6 +95,7 @@ const Post: NextPage<{ postData: unknown }> = ({ postData }) => {
             h5: Heading,
             h6: Heading,
             hr: Divider,
+            code: Code,
           }}
         >
           {postData.content}
