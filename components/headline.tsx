9725edb1050c061c21f526a79ff20e50940dcea8
feightwywx <kururinmiracle@outlook.com>
2022-12-02 23:08:12 +0800

    add(HeadLine): improved responsive headline

--- components/headline.tsx
+++ components/headline.tsx
@@ -1,8 +1,4 @@
-import {
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
 import headbg from "../public/nathan-duck-Jo5FUEkhB_4-unsplash.jpg";
 import darkheadbg from "../public/nathan-duck-Jo5FUEkhB_4-unsplash-dark.jpg";
@@ -13,10 +9,10 @@ export const HeadLine: React.FC<{
 }> = ({ title, subtitle }) => {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   const theme = useTheme();
-  const mobile = useMediaQuery(theme.breakpoints.down("md"));
+  const mobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
-    <div
+    <Box
       style={{
         width: "auto",
         backgroundColor: prefersDarkMode
@@ -25,20 +21,20 @@ export const HeadLine: React.FC<{
         color: prefersDarkMode
           ? theme.palette.surface.on
           : theme.palette.surface.variant,
-        padding: mobile ? "20vh 5%" : "20vh 5%",
-        maxHeight: "60vw",
         borderRadius: "28px",
         backgroundImage: `url(${
           prefersDarkMode ? darkheadbg.src : headbg.src
         })`,
         backgroundSize: "cover",
       }}
+      sx={{ p: { xs: "10vh 5% 2em", sm: "10vh 5%", md: "20vh 5%" } }}
     >
       <Typography
         variant="h1"
         style={{
-          ...theme.typography.displayLarge,
-          fontSize: 64,
+          ...(mobile
+            ? theme.typography.displaySmall
+            : theme.typography.displayLarge),
           fontWeight: 600,
           fontFamily: "Manrope",
           textOverflow: "clip",
@@ -46,9 +42,17 @@ export const HeadLine: React.FC<{
       >
         {title}
       </Typography>
-      <Typography variant="h5" style={theme.typography.displaySmall} sx={{mt: '0.5em'}}>
+      <Typography
+        variant="h5"
+        style={
+          mobile
+            ? theme.typography.headlineSmall
+            : theme.typography.headlineLarge
+        }
+        sx={{ mt: { xs: "1em", sm: "0.5em" } }}
+      >
         {subtitle}
       </Typography>
-    </div>
+    </Box>
   );
 };
